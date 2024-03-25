97747fb4bc405e2558ac452e0b21a7c92675e79e
Javier <10602583+jheard901@users.noreply.github.com>
2024-03-25 14:23:17 -0400

    added logic for axes to reset x pos once offscreen

--- axegame.cpp
+++ axegame.cpp
@@ -75,38 +75,44 @@ class Axe
 {
 protected:
     int axeSizeX, axeSizeY; //size
-    int axeX, axeY; //position
-    int axeDirection; //for movement
-    int isx, isy, ix, iy, id; //initial values
+    double axeX; int axeY; //position
+    int axeDirection; //for up/down movement
+    float axeSpeed; //for left/right movement
+    int isx, isy, ix, iy, id; float is; //initial values    
     int axeLeftEdge, axeRightEdge, axeTopEdge, axeBotEdge; //collision edges
+    bool bOffscreen;
 public:
     Axe()
     {
-        isx = 30; isy = 50; ix = 0; iy = 0; id = 10;
+        isx = 30; isy = 50; ix = 0; iy = 0; id = 10; is = -0.6;
         axeSizeX = isx;
         axeSizeY = isy;
         axeX = ix;
         axeY = iy;
         axeDirection = id;
+        axeSpeed = is;
         axeLeftEdge = axeX;
         axeRightEdge = axeX + axeSizeX;
         axeTopEdge = axeY;
         axeBotEdge = axeY + axeSizeY;
+        bOffscreen = false;
     }
-    Axe(int sizeX, int sizeY, int posX, int posY, int dir)
+    Axe(int sizeX, int sizeY, int posX, int posY, int dir, float spd)
     {
-        isx = sizeX; isy = sizeY; ix = posX; iy = posY; id = dir;
+        isx = sizeX; isy = sizeY; ix = posX; iy = posY; id = dir; is = spd;
         axeSizeX = isx;
         axeSizeY = isy;
         axeX = ix;
         axeY = iy;
         axeDirection = id;
+        axeSpeed = is;
         axeLeftEdge = axeX;
         axeRightEdge = axeX + axeSizeX;
         axeTopEdge = axeY;
         axeBotEdge = axeY + axeSizeY;
+        bOffscreen = false;
     }
-    int GetAxeX() { return axeX; }
+    double GetAxeX() { return axeX; }
     int GetAxeY() { return axeY; }
     int GetAxeSizeX() { return axeSizeX; }
     int GetAxeSizeY() { return axeSizeY; }
@@ -115,7 +121,10 @@ public:
     int GetAxeTopEdge() { return axeTopEdge; }
     int GetAxeBotEdge() { return axeBotEdge; }
     int GetAxeDirection() { return axeDirection; }
+    bool GetAxeOffscreen() { return bOffscreen; }
+    float GetAxeSpeed() { return axeSpeed; }
     void SetAxeDirection(int newDirection) { axeDirection = newDirection; }
+    void SetAxeOffscreen(bool val) { bOffscreen = val; }
     bool IsOutofBounds(int upperBounds, int lowerBounds)
     {
         if(axeY > upperBounds || axeY < lowerBounds)
@@ -127,8 +136,20 @@ public:
             return false;
         }
     }
+    bool IsOffscreen(int bounds)
+    {
+        if (axeX < bounds)
+        {
+            return true;
+        }
+        else
+        {
+             return false;
+        }
+    }
     void UpdatePosition()
     {
+        axeX += axeSpeed;
         axeY += axeDirection;
     }
     void UpdateCollisions()
@@ -146,12 +167,17 @@ public:
         axeY = iy;
         axeDirection = id;
     }
+    void ResetPosX()
+    {
+        axeX = ix;
+    }
 };
 
 int main()
 {
     // size for window
     int sizeX = 690; int sizeY = 420;
+
     // circle/player info (circle drawn from its midpoint)
     float circleSize = 25.0;
     int circleInitialX = int(sizeX*0.1); int circleInitialY = int(sizeY*0.5); //initial position
@@ -161,10 +187,14 @@ int main()
     int circleTopEdge{circleY-int(circleSize)};
     int circleBotEdge{circleY+int(circleSize)};
     Color playerColor{200, 210, 190, 255};
+
     // axes info
-    Axe axe1(30, 50, int(sizeX*0.8), 0, 10);
-    Axe axe2(100, 60, int(sizeX*0.25), 0, 6);
-    Axe axe3(40, 60, int(sizeX*0.6), sizeY-60, -10);
+    int AXE_SIZE = 60; //vertical size
+    int MAX_AXE_WIDTH = 120, MIN_AXE_WIDTH = 4; //horizontal size
+    float MAX_AXE_SPEED = -0.3, MIN_AXE_SPEED = -0.12; //-1.0 is too fast, -0.08 feels too slow
+    Axe axe1(30, 50, int(sizeX+10), 0, 10, MAX_AXE_SPEED*4); //sizeX*0.8
+    Axe axe2(MAX_AXE_WIDTH, AXE_SIZE, int(sizeX*0.25), 0, 6, MAX_AXE_SPEED);
+    Axe axe3(MIN_AXE_WIDTH, AXE_SIZE, int(sizeX*0.6), sizeY-60, -10, MIN_AXE_SPEED);
     Color axeColor{150, 190, 130, 255};
 
     // game vars {} is called braced initialization
@@ -174,7 +204,7 @@ int main()
 
     InitWindow(sizeX, sizeY, "me axe game");
     SetTargetFPS(60);
-    StartTimer(&gameTime, 120.0);
+    StartTimer(&gameTime, 60.0);
 
     while (!WindowShouldClose())
     {
@@ -187,11 +217,15 @@ int main()
         if(IsTimerDone(gameTime))
         {
             //the player has won!
+            DrawText("DEBUG: TIMER DONE", sizeX/2 - gameOverFontSize*3, sizeY/2 - gameOverFontSize, gameOverFontSize, BLACK);
 
             //do something, show victory screen            
         }
         else
         {
+                       
+
+
             //game proceeds until timer done
             if(bCollisionWithAxe)
             {
@@ -232,16 +266,49 @@ int main()
                 DrawRectangle(axe2.GetAxeX(), axe2.GetAxeY(), axe2.GetAxeSizeX(), axe2.GetAxeSizeY(), axeColor);
                 DrawRectangle(axe3.GetAxeX(), axe3.GetAxeY(), axe3.GetAxeSizeX(), axe3.GetAxeSizeY(), axeColor);
 
+
+                //check phase based off timer.lifeTime
+
+                //check bOffscreen for axes to tell if they are offscreen
+                //when true then if the phase has changed then it can be updated
+                //once it return to the right side of the screen
+
+                //need a horizontal direction now for moving them and the checks
+                //for when they hit boundaries
+
+                //the -x position could be based off a variable in main called
+                //MAX_AXE_SIZE and if axe goes past the MAX size then it can be called
+                //back to the right side 
+
+
+
+                //check if phase timer has shifted, if so, then
+                //once axe is offscreen it can be updated to match
+                //size/speed for its next phase
+
+                if(axe1.GetAxeOffscreen())
+                {
+                    //should prob reset PosY also, then set size, speed, and direction
+                    //based off the phase timer
+                    axe1.ResetPosX();                    
+                    axe1.SetAxeOffscreen(false);
+                }
+                
                 //move the axes
                 axe1.UpdatePosition();
                 axe2.UpdatePosition();
                 axe3.UpdatePosition();
 
-                //determine axe directons
+                //determine axe directon and speed
                 if(axe1.IsOutofBounds(sizeY, 0))
                 {
                     axe1.SetAxeDirection(-axe1.GetAxeDirection());
                 }
+                if(axe1.IsOffscreen(-MAX_AXE_WIDTH))
+                {
+                    axe1.SetAxeOffscreen(true);
+                }
+
                 if(axe2.IsOutofBounds(sizeY, 0))
                 {
                     axe2.SetAxeDirection(-axe2.GetAxeDirection());
@@ -262,10 +329,14 @@ int main()
                 }
 
                 //update collision positions
+
+                //the player
                 circleLeftEdge = circleX - circleSize;
                 circleRightEdge = circleX + circleSize;
                 circleTopEdge = circleY - circleSize;
                 circleBotEdge = circleY + circleSize;
+
+                //the axes
                 axe1.UpdateCollisions();
                 axe2.UpdateCollisions();
                 axe3.UpdateCollisions();
